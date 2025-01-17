4e4bafd0b350e81f49211e23cabc907153fc4d11
Wiktoria <wiktoriabanach@op.pl>
2025-01-17 16:14:26 +0100

    Add operations
    Add operation counting sequence length
    Add operation counting amount of each nucleotides
    Add operation counting GC % in sequence

--- DNA Analyzer with Biopython/main.py
+++ DNA Analyzer with Biopython/main.py
@@ -1,4 +1,5 @@
 from Bio.Seq import Seq
+from Bio.SeqUtils import gc_fraction
 
 def app():
     sequence = input("Type in your DNA sequence: ").strip().upper()
@@ -10,11 +11,20 @@ def app():
     while True:
         print("\nHere are operations you can make:")
         print("1. Get complementary sequence")
+        print("2. Get sequence length")
+        print("3. Get amount of each nucleotides")
+        print("4. Get GC %")
         operation = int(input("What would you like to do? Choose number from options: "))
         print("--------------------------------------------------------")
 
         if operation == 1:
             print(seq.complement())
+        elif operation == 2:
+            print(len(seq))
+        elif operation == 3:
+            print(f"A: {seq.count("A")}\nT: {seq.count("T")}\nC: {seq.count("C")}\nG: {seq.count("G")}")
+        elif operation == 4:
+            print(round(100 * (gc_fraction(seq)), 1))
 
         print("--------------------------------------------------------")    
         go_on = input("\nWould you like to make more operations?(y/n): ").upper()
