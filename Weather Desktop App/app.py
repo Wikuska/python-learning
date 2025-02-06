d87e9ed742577cd38a24968d317949b465611777
Wiktoria <wiktoriabanach@op.pl>
2025-02-06 19:36:04 +0100

    Create function to get weather icon based on id

--- Weather Desktop App/app.py
+++ Weather Desktop App/app.py
@@ -1,6 +1,6 @@
 from PySide6.QtCore import Qt
 from PySide6.QtWidgets import QMainWindow, QApplication, QWidget, QVBoxLayout, QLineEdit, QPushButton, QLabel, QComboBox
-from funtionality import get_weather
+from funtionality import get_weather, get_icon
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -8,6 +8,7 @@ class MainWindow(QMainWindow):
 
         self.setWindowTitle("Weather App")
         self.setFixedSize(380, 480)
+        self.setStyleSheet("background-color: #a2d2ff")
 
         layout = QVBoxLayout()
         layout.setContentsMargins(20,20,20,20)
@@ -15,18 +16,23 @@ class MainWindow(QMainWindow):
         top_layout = QVBoxLayout()
         output_layout = QVBoxLayout()
 
-        self.city_input = QLineEdit(self)
+        main_label = QLabel("Enter city name", self)
+
+        self.city_input = QLineEdit(self, alignment=Qt.AlignmentFlag.AlignHCenter)
         self.city_input.setPlaceholderText("Enter city name")
+        self.city_input.setStyleSheet("background-color: white")
         self.city_input.setFixedHeight(30)
 
         self.get_weather_button = QPushButton("Get Weather", self)
         self.get_weather_button.setFixedSize(150, 30)
+        self.get_weather_button.setStyleSheet("background-color: white")
         self.get_weather_button.clicked.connect(self.get_weather)
 
         self.icon_label = QLabel(self)
         self.temp_label = QLabel(self)
         self.weather_label = QLabel(self)
 
+        top_layout.addWidget(main_label)
         top_layout.addWidget(self.city_input)
         top_layout.addWidget(self.get_weather_button, alignment=Qt.AlignmentFlag.AlignHCenter)
 
@@ -47,7 +53,7 @@ class MainWindow(QMainWindow):
         if self.city_input.text() == "": 
             return
         weather_id, weather, temp_in_c = get_weather(self.city_input.text())
-        self.icon_label.setText(f"ID {weather_id}")
+        self.icon_label.setPixmap(get_icon(weather_id))
         self.temp_label.setText(f"Temperature: {temp_in_c}°C")
         self.weather_label.setText(f"Weather: {weather.capitalize()}")
 
--- Weather Desktop App/funtionality.py
+++ Weather Desktop App/funtionality.py
@@ -1,5 +1,7 @@
 import requests
 import os
+from io import BytesIO
+from PySide6.QtGui import QPixmap
 
 API_KEY = os.getenv("OW_API_KEY")
 
@@ -16,4 +18,32 @@ def get_weather(city):
     weather = data["weather"][0]["description"]
     temp_in_c = round(data["main"]["temp"] - 273.15)
     return weather_id, weather, temp_in_c
+
+icon_dict = {
+    2: "11d",
+    3: "09d",
+    50: "10d",
+    51: "13d",
+    52: "09d",
+    53: "09d",
+    6: "13d",
+    7: "50d",
+    800: "01d",
+    801: "02d",
+    802: "03d",
+    803: "04d",
+    804: "04d"
+
+}
+
+def get_icon(id):
+    weather_id_str = str(id)
+
+    for key in icon_dict:
+        if weather_id_str.startswith(str(key)):
+           response = requests.get(f"https://openweathermap.org/img/wn/{icon_dict[key]}@2x.png")
+           image_data = BytesIO(response.content)
+           pixmap = QPixmap()
+           pixmap.loadFromData(image_data.read())
+           return pixmap
 
