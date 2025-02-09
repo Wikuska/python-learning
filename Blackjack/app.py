ae219cf3dddc085105e8994b79fe9cea86a8442c
Wiktoria <wiktoriabanach@op.pl>
2025-02-09 18:33:22 +0100

    Create functions to draw cards and count current score

--- Blackjack/app.py
+++ Blackjack/app.py
@@ -2,6 +2,7 @@ from PySide6.QtCore import Qt
 from PySide6.QtWidgets import  QApplication, QLineEdit, QLabel, QWidget, QScrollArea, QComboBox, QPushButton, QCheckBox, QSpacerItem, QSizePolicy, QMessageBox, QStackedWidget
 from PySide6.QtWidgets import QVBoxLayout, QHBoxLayout
 from PySide6.QtWidgets import QStyleFactory
+from functionality import get_random_card, count_score
 
 class MainWindow(QWidget):
     def __init__(self):
@@ -11,52 +12,62 @@ class MainWindow(QWidget):
         self.setFixedSize(880, 510)
         self.setStyleSheet("background-color: #00994C")
 
+        self.dealer_ranks = []
+        self.player_ranks = []
+
         self.dealer_score = 0
         self.player_score = 0
 
         main_layout = QVBoxLayout(self)
         score_layout = QHBoxLayout()
-        dealer_layout = QHBoxLayout()
-        player_layout = QHBoxLayout()
+        score_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.dealer_layout = QHBoxLayout()
+        self.dealer_layout.setAlignment(Qt.AlignmentFlag.AlignVCenter | Qt.AlignmentFlag.AlignLeft)
+        self.player_layout = QHBoxLayout()
+        self.player_layout.setAlignment(Qt.AlignmentFlag.AlignVCenter | Qt.AlignmentFlag.AlignLeft)
         buttons_layout = QHBoxLayout()
 
-        dealer_score_label = QLabel(f"Dealer Score: {self.dealer_score}")
-        dealer_score_label.setStyleSheet("font-size: 15px;")
-        player_score_label = QLabel(f"Your Score: {self.player_score}")
-        player_score_label.setStyleSheet("font-size: 15px;")
+        self.dealer_score_label = QLabel(f"Dealer Score: 0")
+        self.dealer_score_label.setStyleSheet("font-size: 15px;")
+        self.player_score_label = QLabel(f"Your Score: 0")
+        self.player_score_label.setStyleSheet("font-size: 15px;")
 
-        example_dealer_card = QLabel("6 ♡", alignment = Qt.AlignmentFlag.AlignCenter)
-        example_dealer_card.setFixedSize(100,170)
-        example_dealer_card.setStyleSheet("font-size: 20px; color: red; background-color: white")
-        example_dealer_card.setFrameShape(QLabel.Box)
-        example_dealer_card.setLineWidth(3)
-
-        example_player_card = QLabel("K ♧", alignment = Qt.AlignmentFlag.AlignCenter)
-        example_player_card.setFixedSize(100,170)
-        example_player_card.setStyleSheet("font-size: 20px; color: black; background-color: white")
-        example_player_card.setFrameShape(QLabel.Box)
-        example_player_card.setLineWidth(3)
+        self.set_cards()
 
         hit_button = QPushButton("HIT")
+        hit_button.clicked.connect(lambda: self.draw_card(1))
         stand_button = QPushButton("STAND")
 
-        score_layout.addWidget(dealer_score_label)
-        score_layout.addWidget(player_score_label)
-        score_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
-
-        dealer_layout.addWidget(example_dealer_card, alignment = Qt.AlignmentFlag.AlignVCenter | Qt.AlignmentFlag.AlignLeft)
-
-        player_layout.addWidget(example_player_card, alignment = Qt.AlignmentFlag.AlignVCenter | Qt.AlignmentFlag.AlignLeft)
+        score_layout.addWidget(self.dealer_score_label)
+        score_layout.addWidget(self.player_score_label)
 
         buttons_layout.addWidget(hit_button)
         buttons_layout.addWidget(stand_button)
 
         main_layout.addLayout(score_layout, stretch=0)
-        main_layout.addLayout(dealer_layout, stretch=1)
+        main_layout.addLayout(self.dealer_layout, stretch=1)
         main_layout.addSpacing(20)
-        main_layout.addLayout(player_layout, stretch=1)
+        main_layout.addLayout(self.player_layout, stretch=1)
         main_layout.addLayout(buttons_layout, stretch=0)
         main_layout.setAlignment(buttons_layout, Qt.AlignmentFlag.AlignVCenter)
+
+    def set_cards(self):
+        get_random_card(self.dealer_layout, True)
+        self.draw_card(0)
+        self.draw_card(1)
+
+    def draw_card(self, player):
+        if player == 1:
+            self.player_ranks.append(get_random_card(self.player_layout, False))
+            self.player_score = count_score(self.player_ranks)
+        else:
+            self.dealer_ranks.append(get_random_card(self.dealer_layout, False))
+            self.dealer_score = count_score(self.dealer_ranks)
+        self.update_score()
+
+    def update_score(self):
+        self.dealer_score_label.setText(f"Dealer Score: {self.dealer_score}")
+        self.player_score_label.setText(f"Your Score: {self.player_score}")
 
 
 if __name__ == "__main__":
---
+++ Blackjack/functionality.py
@@ -0,0 +1,52 @@
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import  QApplication, QLineEdit, QLabel
+import random
+
+suits = ['♠', '♣', '♦', '♥']
+values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+deck = [f"{value} {suit}" for suit in suits for value in values]
+
+
+def get_random_card(layout, hidden):
+    if hidden:
+        card = QLabel(alignment = Qt.AlignmentFlag.AlignCenter)
+        card.setFixedSize(100,170)
+        card.setStyleSheet(f"font-size: 20px; color: black; background-color: gray")
+        card.setFrameShape(QLabel.Box)
+        card.setLineWidth(3)
+
+        layout.addWidget(card)
+        return
+
+    random.shuffle(deck)
+    drawn_card = deck.pop()
+
+    if drawn_card.split()[1] == '♥' or drawn_card.split()[1] == '♦':
+        color = "red"
+    else:
+        color = "black"
+
+    card = QLabel(drawn_card, alignment = Qt.AlignmentFlag.AlignCenter)
+    card.setFixedSize(100,170)
+    card.setStyleSheet(f"font-size: 20px; color: {color}; background-color: white")
+    card.setFrameShape(QLabel.Box)
+    card.setLineWidth(3)
+
+    layout.addWidget(card)
+
+    return drawn_card.split()[0]
+
+def count_score(ranks):
+    score = 0
+
+    for rank in ranks:
+        try:
+            points = int(rank)
+        except ValueError:
+            if rank == "A":
+                points = 11
+            else:
+                points = 10
+        score += points
+
+    return score
